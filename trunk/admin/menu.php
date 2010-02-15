d06103e364e7a2cf6a02527d3f2d0984d9560de4
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-15 14:35:40 +0000

    Reorganizing Modules

---
+++ trunk/admin/menu.php
@@ -0,0 +1,10 @@
+<?php
+
+global $adminmenu;
+$adminmenu = array();
+$adminmenu[1]['title'] = _MI_REDIRECT_ADMENU1;
+$adminmenu[1]['link'] = "admin/index.php?op=new";
+$adminmenu[2]['title'] = _MI_REDIRECT_ADMENU2;
+$adminmenu[2]['link'] = "admin/index.php?op=list";
+
+?>
