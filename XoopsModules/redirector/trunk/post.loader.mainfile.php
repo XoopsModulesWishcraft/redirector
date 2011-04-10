cc2fc118f1b4773885ba0e186401833d87b977f8
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-04-10 02:57:49 +0000

    Removing Trunk from Chronolabs Modules

--- XoopsModules/redirector/trunk/post.loader.mainfile.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$redirect_handler = xoops_getmodulehandler('redirection', 'redirector');
-	$redirects = $redirect_handler->getObjects(NULL);	
-	
-	foreach ($redirects as $redirect)
-		$redirect_handler->checkRedirection($redirect);
-
-?>
